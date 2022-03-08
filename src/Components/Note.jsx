3e41878173574b529cf8a04226f631d4f04cce04
Sad7920 <87930878+Sad7920@users.noreply.github.com>
2022-03-08 22:18:55 +0530

    Initial Commit

---
+++ src/Components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return <div className="note">
+        <h1>Note Title</h1>
+        <p>Note Description</p>
+    </div>
+}
+
+export default Note;
